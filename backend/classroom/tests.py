c9242bce6f74209e6ac8d4bd96471d87cc5448b6
Feri Lukmansyah <tkjasikk@gmail.com>
2022-03-19 00:34:23 +0700

    Add Basic Test

--- backend/classroom/tests.py
+++ backend/classroom/tests.py
@@ -7,5 +7,6 @@ class TestStudentModel(TestCase):
         student1 = Student.objects.create(first_name="John", last_name="doe", admission_number=1)
         payload = Student.objects.last()
 
+        # self test
         self.assertEqual(payload.first_name, "John")
         self.assertEqual(payload.last_name, "doe")
