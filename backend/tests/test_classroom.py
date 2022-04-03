d8aaeac3657d9bb89518478bb3a73271c2d46f12
Feri Lukmansyah <tkjasikk@gmail.com>
2022-04-03 13:01:43 +0700

    Done API Student Test

--- backend/tests/test_classroom.py
+++ backend/tests/test_classroom.py
@@ -1,60 +1,34 @@
-import random
 from django.test import TestCase
+from django.urls import reverse
 from mixer.backend.django import mixer
+from rest_framework.test import APIClient
+from rest_framework import status
+
 
 from apps.classroom.models import Classroom
-from apps.classroom.models import Student
 
-# Create your tests here.
-class TestStudentModel(TestCase):
+class TestClassroomAPIView(TestCase):
+    def setUp(self) -> None:
+        self.client = APIClient()
+        return super().setUp()
+
+    def test_classroom_queryset_empty_data(self):
+        # sourcery skip: class-extract-method
+
+        url = reverse("class_qs_api", kwargs={"student_capacity": 25})
+        response = self.client.get(url)
+        assert response.status_code == status.HTTP_202_ACCEPTED
+        payload = response.json()
+        assert payload['classroom_data'] == []
+        assert payload['number_of_classes'] == 0
     
-    def setUp(self) -> None:
-        self.student1 = mixer.blend(Student, first_name="John", last_name="doe")
-        return super().setUp()
+    def test_classroom_queryset_data(self):
+        classroom = mixer.blend(Classroom, student_capacity=25)
+        url = reverse("class_qs_api", kwargs={"student_capacity": 25})
+        response = self.client.get(url)
+        assert response.status_code == status.HTTP_202_ACCEPTED
+        payload = response.json()
+        assert payload['classroom_data'][0]['student_capacity'] == 25
+        assert payload['number_of_classes'] == 1
+        
 
-    def test_create_student(self):
-        payload = Student.objects.last()
-
-        # test data
-        self.assertEqual(payload.first_name, "John")
-        self.assertEqual(payload.last_name, "doe")
-
-    def test_student_can_be_created(self):
-        assert self.student1.first_name == "John"
-
-    def test_pass_grade(self):
-        student = mixer.blend(
-            Student, first_name="Tom", averrage_score=random.randint(50, 60)
-        )
-        self.assertEqual(student.get_grade(), "Pass")
-
-    def test_excelent_grade(self):
-        student = mixer.blend(
-            Student, first_name="Tom", averrage_score=random.randint(70, 99)
-        )
-        self.assertEqual(student.get_grade(), "Excellent")
-
-    def test_failed_grade(self):
-        student = mixer.blend(
-            Student, first_name="Tom", averrage_score=random.randint(1, 40)
-        )
-        assert student.get_grade() == "Failed"
-
-    def test_return_correct_name(self):
-        assert str(self.student1) == "John"
-
-    def test_error_grade(self):
-        student = mixer.blend(
-            Student, first_name="Tom", averrage_score=random.randint(110, 120)
-        )
-        assert student.get_grade() == "Error"
-
-
-class TestClassroomModel(TestCase):
-    def setUp(self) -> None:
-        self.classroom = mixer.blend(Classroom, name="IT")
-        self.payload = Classroom.objects.last()
-        return super().setUp()
-
-    def test_return_correct_name(self):
-        assert str(self.payload) == "IT"
---
+++ backend/tests/test_student_api.py
@@ -0,0 +1,60 @@
+import random
+from django.test import TestCase
+from mixer.backend.django import mixer
+
+from apps.classroom.models import Classroom
+from apps.classroom.models import Student
+
+# Create your tests here.
+class TestStudentModel(TestCase):
+    
+    def setUp(self) -> None:
+        self.student1 = mixer.blend(Student, first_name="John", last_name="doe")
+        return super().setUp()
+
+    def test_create_student(self):
+        payload = Student.objects.last()
+
+        # test data
+        self.assertEqual(payload.first_name, "John")
+        self.assertEqual(payload.last_name, "doe")
+
+    def test_student_can_be_created(self):
+        assert self.student1.first_name == "John"
+
+    def test_pass_grade(self):
+        student = mixer.blend(
+            Student, first_name="Tom", averrage_score=random.randint(50, 60)
+        )
+        self.assertEqual(student.get_grade(), "Pass")
+
+    def test_excelent_grade(self):
+        student = mixer.blend(
+            Student, first_name="Tom", averrage_score=random.randint(70, 99)
+        )
+        self.assertEqual(student.get_grade(), "Excellent")
+
+    def test_failed_grade(self):
+        student = mixer.blend(
+            Student, first_name="Tom", averrage_score=random.randint(1, 40)
+        )
+        assert student.get_grade() == "Failed"
+
+    def test_return_correct_name(self):
+        assert str(self.student1) == "John"
+
+    def test_error_grade(self):
+        student = mixer.blend(
+            Student, first_name="Tom", averrage_score=random.randint(110, 120)
+        )
+        assert student.get_grade() == "Error"
+
+
+class TestClassroomModel(TestCase):
+    def setUp(self) -> None:
+        self.classroom = mixer.blend(Classroom, name="IT")
+        self.payload = Classroom.objects.last()
+        return super().setUp()
+
+    def test_return_correct_name(self):
+        assert str(self.payload) == "IT"
